31bbf2f96a4579a096203b165bd40ef34958526a
yanin17 <zabakunoyanin@gmail.com>
2024-08-30 08:59:12 +0700

    add ex9

---
+++ Exercise9.java
@@ -0,0 +1,21 @@
+
+// Remove a specific element from an array
+import java.util.Arrays;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        int[] my_array = { 25, 14, 56, 15, 36, 56, 77, 18, 29, 49 };
+
+        System.out.println("Original Array : " + Arrays.toString(my_array));
+
+        // Remove the second elements (index->1, value->14) of the array
+        int removeIndex = 1;
+        for (int i = removeIndex; i < my_array.length - 1; i++) {
+            my_array[i] = my_array[i + 1];
+        }
+
+        // We cannot alter the size of an array, after the removal, the last and second
+        // last element in th array will exist twice
+        System.out.println("After removing the second element : " + Arrays.toString(my_array));
+    }
+}
